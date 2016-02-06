ed707e56a2d722874d23fe68ee17222f9b5960c1
Christopher Rigor <crigor@gmail.com>
2016-02-06 09:52:05 +0800

    Test rpy2

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    version="0.0.0",
+    name="...",
+    packages=find_packages(),
+    install_requires=[
+        ...
+        "green>=2.0.7",
+        "rpy2>=2.7.7",
+    ],
+)
